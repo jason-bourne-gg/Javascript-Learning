d5307fc8414efc7de7a1fed56b14e914dc20b08d
jason-bourne-gg <car10@iitbbs.ac.in>
2023-02-27 14:27:32 +0530

    first function in js

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(" HI, This is a function to add three numnbers ");
+
+function addThreeNumbers (a,b,c) {
+    return a + b + c;
+}
+
+const add = addThreeNumbers (3,4,5);
+
+console.log(" " + add + " "); 
