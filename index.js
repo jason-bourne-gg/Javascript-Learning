f51390f591bde15ccb00f7f079d1217993a65240
jason-bourne-gg <car10@iitbbs.ac.in>
2023-02-27 15:43:29 +0530

    added html slide

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-console.log(" HI, This is a function to add three numnbers ");
+// console.log(" HI, This is a function to add three numnbers ");
 
-function addThreeNumbers (a,b,c) {
-    return a + b + c;
-}
+// function addThreeNumbers (a,b,c) {
+//     return a + b + c;
+// }
 
-const add = addThreeNumbers (3,4,5);
+// const add = addThreeNumbers (3,4,5);
 
-console.log(" " + add + " "); 
+// console.log(" " + add + " "); 
+
+console.log(a);
+console.log(b);
+var a = 4
+let b = 8
+
+// let c = 6
+// let b = 10
+
+// console.log(" " + a,b + " ");
